0d9103621e971418aeec006b674e9eb91f0e2d8b
thesuperdave <davetad903@gmail.com>
2021-02-24 11:02:13 -0600

    added Week 3 Assignemnt 2

--- Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
+++ Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
@@ -2,10 +2,33 @@ package week3;
 
 import acm.program.ConsoleProgram;
 
+/*
+Write a method
+	public String removeAllOccurrences(String str, char ch)
+that removes all occurrences of the character ch from the string str. For example, your
+method should return the values shown:
+
+removeAllOccurrences("This is a test", 't') returns "This is a es"
+removeAllOccurrences("Summer is here!", 'e') returns "Summr is hr"
+removeAllOccurrences("---0---", '-') returns "0"
+*/
+
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		println(removeAllOccurrences("This is a test", 't')); // returns "This is a es"
+		println(removeAllOccurrences("Summer is here!", 'e')); // returns "Summr is hr"
+		println(removeAllOccurrences("---0---", '-')); // returns "0"
 	}
+	
+	public String removeAllOccurrences(String str, char ch)
+	{
+		String charToRemove = Character.toString(ch);
+		//print(charToRemove);
+		String newString = str.replace(charToRemove, "");
+		//print(newString);
+		return newString;
+	}
+
 	
 }
