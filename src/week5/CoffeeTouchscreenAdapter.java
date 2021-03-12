c48c25c85adff87dcbfa79ac69bdd49fb4573c7f
thesuperdave <davetad903@gmail.com>
2021-03-12 09:39:17 -0600

    fixed bugs in Adapter Pattern assignment

--- src/week5/CoffeeTouchscreenAdapter.java
+++ src/week5/CoffeeTouchscreenAdapter.java
@@ -2,22 +2,22 @@ package week5;
 
 public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface
 {
-	private OldCoffeeMachine oldMachine;
-
-	public void CoffeeTouchScreenAdapter()
+	OldCoffeeMachine oldMachine;
+	
+	public CoffeeTouchscreenAdapter(OldCoffeeMachine machine)
 	{
-		this.oldMachine = new OldCoffeeMachine();
+		oldMachine = machine;
 	}
-
+	
 	public void chooseFirstSelection()
 	{
 		System.out.println("Old Machine option A was selected via adapter");
-		this.oldMachine.selectA();		
+		oldMachine.selectA();
 	}
 
 	public void chooseSecondSelection()
 	{
 		System.out.println("Old Machine option B was selected via adapter");
-		this.oldMachine.selectB();		
+		oldMachine.selectB();
 	}
 }
--- src/week5/TestCoffeeMachine.java
+++ src/week5/TestCoffeeMachine.java
@@ -5,7 +5,8 @@ public class TestCoffeeMachine {
 	public static void main(String[] arg)
 	{	
 		// OldCoffeeMachine oldM = new OldCoffeeMachine();
-		CoffeeTouchscreenAdapter machineAdapter = new CoffeeTouchscreenAdapter();		
+		CoffeeMachineInterface machineAdapter = new CoffeeTouchscreenAdapter(new OldCoffeeMachine());
+		
 		machineAdapter.chooseFirstSelection();
 		machineAdapter.chooseSecondSelection();
 	}
