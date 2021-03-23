2b984b878b5931d4a1065b787a71649de09b000b
thesuperdave <davetad903@gmail.com>
2021-03-22 20:58:53 -0500

    adding week7 assignments

---
+++ src/week5AdapterPattern/CoffeeMachineTesting.java
@@ -0,0 +1,26 @@
+package week5AdapterPattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+// import org.junit.jupiter.api.Test;
+
+class CoffeeMachineTesting {
+	/*
+	@Test
+	void testOldCoffeeMachine() {
+		OldCoffeeMachine machine = new OldCoffeeMachine();
+		
+		assertEquals("Option A was Selected from Old Machine", machine.selectA());
+		assertEquals("Option B was Selected from Old Machine", machine.selectB());
+	}
+
+	@Test
+	void testCoffeeMachineInterface() {
+		CoffeeMachineInterface interface = new CoffeeMachineInterface();
+		
+		assertEquals("Option A was Selected from Old Machine", machine.selectA());
+		assertEquals("Option B was Selected from Old Machine", machine.selectB());
+	}
+	*/
+}
